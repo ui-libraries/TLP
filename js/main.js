30a51a8c71899ee83c04d4bc6f14510c1ac9cccc
Matthew Butler <matthewtbutler@gmail.com>
2016-08-05 16:11:32 -0500

    prototractatus
    updated prototractatus from may 05 spreadsheet

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var gap = 50;
 var version = '.ger';
 var lineGroup = [];
 var divCounter = 0; //append to div name to create unique ids every time
+var i = 17;
 
 
 var width = $("#map").width();
@@ -24,25 +25,31 @@ var elem = svg.selectAll("g")
 var elemLine = svg.selectAll("g")
     .data(lines);
 
-/* Define the data for the curves */
+/* Define the data for the curves 
 var elemCurve = svg.selectAll("g")
-    .data(curves);
+    .data(curves); */
 
+/* Define the data for the end caps 
 var elemEnd = svg.selectAll("g")
-    .data(ends);
+    .data(ends);*/
 
+/* Define the data for the non-junction stops */
 var elemRect = svg.selectAll("g")
     .data(rectangles);
 
+/*Create and place the "blocks" containing the lines */
 var elemLineEnter = elemLine.enter()
     .append("g");
 
+/*Create and place the "blocks" containing the curves 
 var elemCurveEnter = elemCurve.enter()
-    .append("g");
+    .append("g");*/
 
+/*Create and place the "blocks" containing the rectangles and the text 
 var elemRectEnter = elemRect.enter()
-    .append("g");
+    .append("g"); */
 
+/*Create and place the "blocks" containing the end caps and the text 
 var elemEndEnter = elemEnd.enter()
     .append("g");
 
@@ -50,6 +57,7 @@ var elemEndEnter = elemEnd.enter()
 var elemEnter = elem.enter()
     .append("g");
 
+/*Create the line */
 var line = elemLineEnter.append("line")
     .attr("x1", function (d) {var point = findPoints(d); return point.x1 * gap; }) //x_axis of 1st section + radius/2 ?
     .attr("y1", function (d) {var point = findPoints(d); return point.y1 * gap; }) //y_axis of 1st section
@@ -59,6 +67,7 @@ var line = elemLineEnter.append("line")
     .attr("stroke", function (d) {return d.color; })
     .on("click", buildGroup);
 
+/*Create the curve 
 var curve = elemCurveEnter.append("line")    
     .attr("x1", function (d) {var point = findPoints(d); return point.x1 * gap; }) //x_axis of 1st section + radius/2 ?
     .attr("y1", function (d) {var point = findPoints(d); return point.y1 * gap; }) //y_axis of 1st section
@@ -66,9 +75,9 @@ var curve = elemCurveEnter.append("line")
     .attr("y2", function (d) {var point = findPoints(d); return point.y2 * gap; }) //y_axis of 2nd section
     .attr("stroke-width", 20)   //double radius?
     .attr("stroke", function (d) {return d.color; })   
-    .on("click", buildGroup);
+    .on("click", buildGroup);*/
 
-/*Create the circle for each block */
+/*Create the circles for junctions */
 var circle = elemEnter.append("circle")
     .attr("cx", function (d) { return d.x_axis * gap; })
     .attr("cy", function (d) { return d.y_axis * gap; })
@@ -78,28 +87,30 @@ var circle = elemEnter.append("circle")
     .attr("stroke-width", 4)
     .on("click", showSection);
 
+/*Create the rectangles for stops 
 var rect = elemRectEnter.append("rect")
     .attr("x", function (d) { return d.x_axis * gap; })
     .attr("y", function (d) { return d.y_axis * gap; })
     .attr("fill", function (d) { return d.color; })
     .attr("width", 25)
     .attr("height", 30)
-    .on("click", showSection);
+    .on("click", showSection);*/
 
+/*Create the end caps 
 var ends = elemEndEnter.append("rect")
     .attr("x", function (d) { return d.x_axis * gap - 1; })
     .attr("y", function (d) { return d.y_axis * gap - 28; })
     .attr("fill", function (d) { return d.color; })
     .attr("width", 25)
     .attr("height", 60)
-    .on("click", showSection);
+    .on("click", showSection);*/
 
-/* Create the text for each block */
+/* Create the text for each stop 
 elemRectEnter.append("text")
     .attr("dx", function (d) {return d.x_axis * gap; })
     .attr("dy", function(d){return d.y_axis * gap - 15; })
     .attr("font-size","16px")
-    .text(function (d) {return d.label});
+    .text(function (d) {return d.label}); */
 
 /* Create the text for each block */
 elemEnter.append("text")
@@ -108,12 +119,15 @@ elemEnter.append("text")
     .attr("font-size","16px")
     .text(function (d) {return d.label});
 
+/* Create the text for each end cap 
 elemEndEnter.append("text")
     .attr("dx", function (d) {return d.x_axis * gap + 30; })
     .attr("dy", function(d){return d.y_axis * gap - 15; })
     .attr("font-size","16px")
-    .text(function (d) {return d.label});
+    .text(function (d) {return d.label});*/
 
+
+//calculates how many decimal points each section number has
 function findPrecision(a) {
     var precision;
     //convert to string and count the length after the decimal point
@@ -126,6 +140,7 @@ function findPrecision(a) {
     return precision;
 }
 
+//for each circle, rectangle, and end cap -- looks up numerical label in the html, builds a jquery dialog box, then populates dialog with relevant section text
 function findSection(section, lang) {
     var sectionNum = '';
 
@@ -146,7 +161,6 @@ function findSection(section, lang) {
         modal: false,
         draggable: true,
         resizable: true,
-        close: closeFunction,
         position: ['center', 'bottom'],        
         height: 500,
         width: 400,
@@ -161,31 +175,32 @@ function findSection(section, lang) {
 
 }
 
-function addDropdown(div) {
-    console.log(div);
-    $(div).append($('<div>').load('lang-version.html'));
-}
-
+//returns a point object with beginning and end sections
 function findPoints(d) {
-    var points = {},
+    var points = {},        
         start = d.start,
         end = d.end,
         startPoint = _.filter(sections, {"label": start}),
         endPoint = _.filter(sections, {"label": end});
     
-    if (end === "1.13") {
+    console.log("line " + i);
+    //temporary workaround for end cap demo. delete this when actual list is working
+    /*if (end === "1.13") {
         endPoint = _.filter(ends, {"label": end});
-    }
-    
+    }*/  
+
     points.x1 = startPoint[0].x_axis;
     points.x2 = endPoint[0].x_axis;
     points.y1 = startPoint[0].y_axis;
     points.y2 = endPoint[0].y_axis;
-    points.color = d.color;
+    points.color = d.color; 
     
+    i++;
+
     return points;
 }
 
+//builds the dialog container then uses findSection to populate
 function showSection(d) {
     
     var label = d.label;
@@ -206,10 +221,7 @@ function showSection(d) {
     findSection(label, version);
 }
 
-function closeFunction(e) {
-    //console.log(e.target.id);
-}
-
+//creates an array of sections when a line is clicked then populates a dialog with each section text
 function buildGroup(d) {    
     var start = d.start,
         end = d.end,
@@ -266,6 +278,7 @@ function buildGroup(d) {
     
 }
 
+//switches the language when language dropdown is changed
 $(document).on('change', ".selectChange", function () {
     var divID = '#' + $(this).parent()[0].offsetParent.id;
     var lang = '.' + this.value;
