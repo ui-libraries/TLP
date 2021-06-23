9352ee9740317878eece2606116f517cb6975e6a
Matthew Butler <matthewtbutler@gmail.com>
2021-06-22 20:12:33 -0500

    add diff and lodash to package.json

--- test/sample.test.js
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai'
-import { sample } from '../src/sample'
-
-describe('Sample', () => {
-    it('outputs the working confirmation message', () => {
-        expect(sample()).to.equal("It works!")
-    })
-})
